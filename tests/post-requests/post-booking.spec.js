7d65924af8731cd4de316007e749845ca0d83cbc
anandavii <anandavii@gmail.com>
2025-06-30 16:47:26 -0500

    added test case for creating a booking

--- tests/post-requests/post-booking.spec.js
+++ tests/post-requests/post-booking.spec.js
@@ -1,26 +1,38 @@
 import { test, expect } from '@playwright/test'
 
 test('POST- Create a new booking', async ({ request }) => {
+    //Booking payload
+    const bookingPayload = {
+        "firstname": "Abhishek",
+        "lastname": "Anand",
+        "totalprice": 1099,
+        "depositpaid": true,
+        "bookingdates": {
+            "checkin": "2025-07-01",
+            "checkout": "2025-07-10"
+        },
+        "additionalneeds": "Lunch and Breakfast"
+    }
 
+    // POST request
     const response = await request.post(`/booking`, {
-        data: {
-            "firstname": "Abhishek",
-            "lastname": "Anand",
-            "totalprice": 1099,
-            "depositpaid": true,
-            "bookingdates": {
-                "checkin": "2025-07-01",
-                "checkout": "2019-07-10"
-            },
-            "additionalneeds": "Lunch and Breakfast"
-        }
+        data: bookingPayload
     })
+
     const responseBody = JSON.parse(await response.text())
 
     //assert status code 200
     expect(response.status()).toBe(200)
     expect(response.ok()).toBe(true)
 
+    // Validate booking id is present
+    expect(responseBody).toHaveProperty('bookingid')
+    expect(typeof responseBody.bookingid).toBe('number')
+    expect(responseBody).toHaveProperty('booking')
 
+    // Validate booking details
+    expect(responseBody.booking).toMatchObject(bookingPayload)
+
+    // log
     console.log(responseBody)
 })
