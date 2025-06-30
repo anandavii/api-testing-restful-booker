55ed5e954c888deca3bd14976a5a847a1a54701b
anandavii <anandavii@gmail.com>
2025-06-30 12:09:37 -0500

    added test to get all the bookings

--- tests/get-requests/get-booking.spec.js
+++ tests/get-requests/get-booking.spec.js
@@ -1,6 +1,12 @@
 import { test, expect } from '@playwright/test'
 
-test('GET- Get the booking details', async ({ request }) => {
+test('GET- Get all the booking details', async ({ request }) => {
     const response = await request.get(`/booking`)
+    const responseBody = JSON.parse(await response.text())
+
+    //assert status code 200
     expect(response.status()).toBe(200)
+
+    //log the response
+    console.log(responseBody)
 })
