b618a8e3056beeb9973a024f378f30f2e1bafb3f
anandavii <anandavii@gmail.com>
2025-06-30 16:18:43 -0500

    updated the config

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,9 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://restful-booker.herokuapp.com',
-
+    extraHTTPHeaders: {
+      Accept: "application/json"
+    },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
---
+++ tests/post-requests/post-booking.spec.js
@@ -0,0 +1,5 @@
+import { test, expect } from '@playwright/test'
+
+test('POST- Create a new booking', async ({ request }) => {
+
+})
