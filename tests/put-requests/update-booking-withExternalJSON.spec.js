ffb6ed94b704e01252bef2e44e148a471fd8a6ee
anandavii <anandavii@gmail.com>
2025-06-30 20:49:44 -0500

    added the test case to update the booking details using external JSON data file

---
+++ tests/put-requests/update-booking-withExternalJSON.spec.js
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test'
+import bookingPayload from '../../testdata/createBookingPayload.json'
+import updateBookingPayload from '../../testdata/updateBookingPayload.json'
+
+test('PUT- Update the booking details using JSON from external File', async ({ request }) => {
+
+    // POST request to create a new booking
+    const response = await request.post(`/booking`, {
+        data: bookingPayload
+    })
+
+    //log the new booking
+    console.log(await response.json())
+
+    // store the booking id for the new booking created
+    const { bookingid } = await response.json()
+
+    // now get the auth token
+    const authRes = await request.post(`/auth`, {
+        data: {
+            username: 'admin',
+            password: 'password123'
+        }
+    })
+
+    // store the auth token
+    const { token } = await authRes.json()
+
+    const updateRes = await request.put(`/booking/${bookingid}`, {
+        headers: {
+            Cookie: `token=${token}`
+        },
+        data: updateBookingPayload
+    })
+
+    // assert status code
+    expect(updateRes.status()).toBe(200)
+    expect(updateRes.ok()).toBe(true)
+
+    const updatedResponseBody = JSON.parse(await updateRes.text())
+
+    // match the updated record
+    expect(updatedResponseBody).toMatchObject(updateBookingPayload)
+})
